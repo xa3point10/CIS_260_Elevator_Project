14c1f76f73f08b5b47e20f8fed53a52eeb70a417
xa3point10 <ixevelazquez@gmail.com>
2017-05-02 12:31:37 -0400

    Update MVC Demo, working

--- CodeFiles/Elevator_v1/src/pckElevator_V1/ElevatorBank.java
+++ CodeFiles/Elevator_v1/src/pckElevator_V1/ElevatorBank.java
@@ -3,6 +3,7 @@ package pckElevator_V1;
 import java.util.ArrayList;
 
 public class ElevatorBank {
+
     // singleton design support
     //------------------------------
     private static Mutex mMutex = new Mutex();
@@ -10,6 +11,9 @@ public class ElevatorBank {
 
     // data
     //------------------------------
+    //private IScenario scenario;
+    // getscenario
+    // setScenario  
     private ArrayList< Elevator> elevators;
     private ArrayList< Floor> floors;
 
@@ -18,11 +22,13 @@ public class ElevatorBank {
     public ElevatorBank() {
         elevators = new ArrayList<>();
         floors = new ArrayList<>();
-    }//ElevatorBank
+    }//ElevatorBank()
 
+    //------------------------------
     // operations
     //------------------------------
-    public static ElevatorBank GetInstance() {
+    // Only allow Access through the mutex Lock
+    public static ElevatorBank GetInstance() { // Access through the mutex Lock
         Lock lock = new Lock(mMutex); // automatically locks
 
         if (mInstance == null) {
@@ -36,6 +42,33 @@ public class ElevatorBank {
     public void addElevator(Elevator elevator) {
         elevators.add(elevator);
     }//addElevator
+
+    public void updateConfiguration(int numberOfFloors, int numberOfElevators, int numberOfVisitors) {
+        elevators.clear();  // use Arraylist Clear() method
+        floors.clear();     // use Arraylist Clear() method
+        
+        // populate the floors
+        for (int idx = 0; idx < numberOfFloors; ++idx) {
+            Floor floor = null;
+            if (idx == 0) {
+                floor = new Floor("P");
+            } else if (idx == 1) {
+                floor = new Floor("L");
+            } else {
+                floor = new Floor(Integer.toString(idx));
+            }
+            addFloor(floor);
+        }
+        // populate the elevators
+        for (int idx = 0; idx < numberOfElevators; ++idx) {
+            Elevator elevator = null; // construct an Elevator Object
+            char elevatorLabel = 'A';
+            elevatorLabel += (char) idx;
+            //Fill that Elevator object
+            elevator = new Elevator(Character.toString(elevatorLabel), idx);
+            addElevator(elevator);
+        }
+    }//updateConfiguration
 
     public Elevator getElevator(int elevatorSeqNumber) {
         return elevators.get(elevatorSeqNumber);
@@ -57,29 +90,4 @@ public class ElevatorBank {
         return floors;
     }//getFloors
 
-    public void updateConfiguration(int numberOfFloors, int numberOfElevators) {
-        elevators.clear();
-        floors.clear();
-        // populate the floors
-        for ( int idx = 0; idx < numberOfFloors; ++idx ) {
-            Floor floor = null;
-            if (idx == 0) {
-                floor = new Floor( "P" );
-            } else if (idx == 1) {
-                floor = new Floor( "G" );
-            } else {
-                floor = new Floor( Integer.toString( idx ) );
-            }
-            addFloor( floor );
-        }
-        // populate the elevators
-        for ( int idx = 0; idx < numberOfElevators; ++idx ) {
-            Elevator elevator = null;
-            char elevatorLabel = 'A';
-            elevatorLabel += (char)idx;
-            elevator = new Elevator( Character.toString( elevatorLabel ), idx );
-            addElevator( elevator );
-        }
-    }//updateConfiguration
-    
 }//class ElevatorBank
